f91d2655eabb9d1d7d088b309e054c3dd6a67376
JiJunyang <qwer_5745@qq.com>
2017-06-20 12:58:16 +0800

    模拟冲突用-本地

--- src/com/test/git/TestForGit.java
+++ src/com/test/git/TestForGit.java
@@ -5,6 +5,10 @@ public class TestForGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git ");
 		System.out.println("add a line ");
+		
+		int i = 0;
+		
+		System.out.print(i);
 	}
 
 }
