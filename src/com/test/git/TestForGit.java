a71bcfed0519616324f520d98e33ab26db34a25a
瞬时真 <qwer_5745@qq.com>
2017-06-20 12:48:31 +0800

    模拟冲突
    模拟冲突

--- src/com/test/git/TestForGit.java
+++ src/com/test/git/TestForGit.java
@@ -5,6 +5,9 @@ public class TestForGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git ");
 		System.out.println("add a line ");
+		
+		int j = 1;
+		System.out.println(j);
 	}
 
 }
